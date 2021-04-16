8e71fd3346e0488b1611d3c6e91556e455c21741
Walesia Robinson II <lisalynn@terpmail.umd.edu>
2021-04-16 11:40:12 -0400

    added main script we worked on in discussion 4/16

---
+++ bankfile.py
@@ -0,0 +1,26 @@
+"""Create financial summary information from transaction data in a file.
+
+Read transactions, summarize spending over time, plot spending over time, identify suspicious transactions, 
+optionally filter by date range, amount, or category.
+
+Print financial summary to txt file.
+"""
+
+class Bookkeeper(): 
+    """ This class reads the Mint transactions.csv file for use in following modules/functions.
+    
+    Attributes: 
+    filepath (str): path to file containing user's financial transactions.
+
+    """
+    def __init__(self, filepath):
+    """ Opens the user's financial transaction file reading, creates and builds a dataframe from it. 
+    
+    Args: 
+        filepath (file): 
+    """
+    
+    
+ 
+
+
