652fbc72c6417ff6923ba98958716f37cb469e49
Tristan Clark <tristansclark@gmail.com>
2021-05-17 15:34:07 -0400

    Add test script to work on

---
+++ bankfile_test.py
@@ -0,0 +1,12 @@
+"""Tests our bankfile.py code to make sure it operates correctly.
+"""
+import pytest
+import bankfile
+import pandas as pd
+
+def test_search_transactions():
+    """Does Bookkeeper.search_transactions return results from the dataframe
+    based on 
+    """
+    r = bankfile.Bookkeeper("transactions.csv")
+    r2 = r.search_transactions("spotif")
