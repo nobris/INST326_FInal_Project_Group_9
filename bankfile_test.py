64b6e0fc03eb604334ded5d1976f110f9ceddc92
Sophia Chen <chensophiah@gmail.com>
2021-05-19 15:52:13 -0700

    Update bankfile_test.py

--- bankfile_test.py
+++ bankfile_test.py
@@ -17,4 +17,10 @@ def test_spending_category_frequency():
     """
     r = bankfile.Bookkeeper("transactions.csv")
     r2 = r.spending_category_frequency()
-    
+
+def test_day_of_week_summary():
+    """Does Bookkeeper.day_of_week_summary return results from the dataframe
+    based on 
+    """    
+    r = bankfile.Bookkeeper("transactions.csv")
+    r2 = r.day_of_week_summary()   
