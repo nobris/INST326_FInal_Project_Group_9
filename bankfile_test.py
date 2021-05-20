7a8f01f67543ec746d51dd0e39b02f007d61957d
Walesia Robinson II <lisalynn@terpmail.umd.edu>
2021-05-19 21:50:46 -0400

    added tests for suspicious transactions

--- bankfile_test.py
+++ bankfile_test.py
@@ -4,24 +4,64 @@ import pytest
 import bankfile
 import pandas as pd
 
+class TestBookkeeper: 
+    
+    def __init__(self, transactions): 
+        "Mock Bookkeeper object used for testing our functions."
+        self.transactions = pd.read_csv(transactions)
+        
+        # drop empty columns from dataframe
+        self.transactions = self.transactions.drop(["Labels", "Notes"], axis = 1)
+        
+        # change Date column to datetime format
+        self.transactions["Date"] = pd.to_datetime(self.transactions["Date"])
+        
+        # earliest and most recent dates from the user's financial transactions
+        self.earliest = str(min(self.transactions["Date"].dt.date))
+        self.latest = str(max(self.transactions["Date"].dt.date))
+        
+# testing suspicious transactions method
+def test_suspicious_transactions_no_args():
+    """ Checks whether the suspicious transactions method works with no 
+    optional arguments.
+    """ 
+    test = TestBookkeeper("transactions.csv")
+    bankfile.Bookkeeper.suspicious_charges(test)
+    
+def test_suspicious_transactions_one_arg():
+    """ Checks whether the suspicious transactions method works with just 
+    start date.
+    """
+    test2 = TestBookkeeper("transactions.csv")
+    bankfile.Bookkeeper.suspicious_charges(test2, "04-01-2020", "04-30-2020")
+    
+def test_suspcious_transactions_two_args():
+    """ Checks whether the suspicious transactions method works with start 
+    and end dates."
+    """
+    test3 = TestBookkeeper("transactions.csv")
+    bankfile.Bookkeeper.suspicious_charges(test3, "04-01-2020", "04-30-2020")
+
+def test_suspicious_transactions_three_args():
+    """Checks whether the suspicous transactions method works with start date, end date
+    and optional account name specified. 
+    """
+    test4 = TestBookkeeper("transactions.csv")
+    bankfile.Bookkeeper.suspicious_charges(test4, "04-01-2020", "04-30-2020", "Discover")
+
 def test_search_transactions():
-    """Does Bookkeeper.search_transactions return results from the dataframe
-    based on 
+    """Does Bookkeeper.search_transactions return results from the 
+    dataframe based on 
     """
     r = bankfile.Bookkeeper("transactions.csv")
     r2 = r.search_transactions("spotif")
     
-def test_suspicious_transactions():
-    """ This test checks whether the suspicious transactions test is searching
-    all of the user's available accounts.
-    """ 
     
 @pytest.fixture
 def test_spending_category_frequency():
     """Testing fixture for spending_category_frequency
     """
     r = bankfile.Bookkeeper("transactions.csv")
-<<<<<<< HEAD
     r2 = r.spending_category_frequency()
 
 def test_day_of_week_summary():
@@ -30,7 +70,6 @@ def test_day_of_week_summary():
     """    
     r = bankfile.Bookkeeper("transactions.csv")
     r2 = r.day_of_week_summary()   
-=======
     return r.spending_category_frequency()
 
 def test_category_counts(test_spending_category_frequency):
@@ -44,4 +83,3 @@ def test_category_counts(test_spending_category_frequency):
     assert test_spending_category_frequency.loc['Restaurants', 'count'] == df['Category'].value_counts().Restaurants
     assert test_spending_category_frequency.loc['Credit Card Payment', 'count'] ==df.loc[df.Category == "Credit Card Payment", 'Category'].count()
     
->>>>>>> 7811a52e6c15a24c8b344cf5c8150a06e28275c4
