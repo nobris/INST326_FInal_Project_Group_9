8d1e38918584ef2c80e44d7f9813e7e13b4b9476
Sophia Chen <chensophiah@gmail.com>
2021-05-19 18:37:49 -0700

    Update bankfile_test.py

--- bankfile_test.py
+++ bankfile_test.py
@@ -10,41 +10,41 @@ def test_search_transactions():
     """
     r = bankfile.Bookkeeper("transactions.csv")
     r2 = r.search_transactions("spotif")
-
+    
 def test_suspicious_transactions():
     """ This test checks whether the suspicious transactions test is searching
     all of the user's available accounts.
     """ 
 
-<<<<<<< HEAD
-   
+@pytest.fixture   
 def test_day_of_week_summary():
-    """Tests the accuracy of the values of the summary values  
-    """ 
+    r = bankfile.Bookkeeper("transactions.csv")
+    return r.day_of_week_summary()
+
+def test_dows_values(test_day_of_week_summary):
+    """Does day_of_week_summary return the correct values for each summary category?
+    """
+    df = pd.read_csv("transactions.csv")
+    df["Day of Week"] = df["Date"].dt.strftime("%A")
+    
+    assert test_day_of_week_summary.loc['Monday', 'Avg Transactions'] == round(df[df["Day of Week"]=='Monday'].groupby("Date")["Date"].count().mean(),2)
+    assert test_day_of_week_summary.loc['Tuesday', 'Mean'] == round(df[df["Day of Week"]=='Tuesday'].groupby("Date")["Amount"].sum().mean(),2)
+    assert test_day_of_week_summary.loc['Wednesday', 'Median'] == round(df[df["Day of Week"]=='Wednesday'].groupby("Date")["Amount"].sum().median(),2)
+    assert test_day_of_week_summary.loc['Thursday', 'Minimum'] == round(df[df["Day of Week"]=='Thursday'].groupby("Date")["Amount"].sum().min(),2)
+    assert test_day_of_week_summary.loc['Friday', 'Maximum'] == round(df[df["Day of Week"]=='Friday'].groupby("Date")["Amount"].sum().max(),2)
+    
          
-=======
->>>>>>> 9ed0c11917386db8dd1bb2da1f7470e156373a8a
 @pytest.fixture
 def test_spending_category_frequency():
     """Testing fixture for spending_category_frequency
     """
     r = bankfile.Bookkeeper("transactions.csv")
-<<<<<<< HEAD
     r2 = r.spending_category_frequency()
     return r.spending_category_frequency()
     
 def test_category_counts(test_spending_category_frequency):
-=======
-    return r.spending_category_frequency()
-
-def test_counts(test_spending_category_frequency):
-<<<<<<< HEAD
->>>>>>> 9ed0c11917386db8dd1bb2da1f7470e156373a8a
     """Tests whether or not the counts for each category in the frequency table are
     accurate or not
-=======
-    """Does spending_category_frequency return the correct category counts?
->>>>>>> e6403da962f40ba6611c394e83b3daf065cb29ac
     """
     df = pd.read_csv("transactions.csv")
     assert test_spending_category_frequency.loc['Shopping', 'count'] == df['Category'].value_counts().Shopping
@@ -53,13 +53,3 @@ def test_counts(test_spending_category_frequency):
     assert test_spending_category_frequency.loc['Restaurants', 'count'] == df['Category'].value_counts().Restaurants
     assert test_spending_category_frequency.loc['Credit Card Payment', 'count'] ==df.loc[df.Category == "Credit Card Payment", 'Category'].count()
     
-<<<<<<< HEAD
-=======
-def test_day_of_week_summary():
-    """Does Bookkeeper.day_of_week_summary return results from the dataframe
-    based on 
-    """    
-    r = bankfile.Bookkeeper("transactions.csv")
-    r2 = r.day_of_week_summary()   
-    
->>>>>>> 9ed0c11917386db8dd1bb2da1f7470e156373a8a
